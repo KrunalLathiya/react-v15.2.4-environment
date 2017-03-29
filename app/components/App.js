e05e5bf11d0cb5028a1114c7c6a71a7ff4b7a77f
Krunal Lathiya <KRUNAL@DESKTOP-51BTMB4.(none)>
2017-03-30 01:25:47 +0530

    React Development Environment

---
+++ app/components/App.js
@@ -0,0 +1,10 @@
+import React, {Component} from 'react';
+export default class App extends Component {
+    render(){
+        return (
+            <div>
+                Hello From React v15.4.2
+            </div>
+        );
+    }
+}
